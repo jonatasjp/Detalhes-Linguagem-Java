a48024c034eb5c240666a218fb03a81f670e0a06
Jonatas Luis <jonatasluis.unipe@gmail.com>
2017-12-20 00:02:25 -0200

    usando matches

--- src/regex/UsandoPatternEMatcher.java
+++ src/regex/UsandoPatternEMatcher.java
@@ -1,14 +1,29 @@
 package regex;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class UsandoPatternEMatcher {
 
 	public static void main(String[] args) {
+		
+		Pattern expression = Pattern.compile("J.*\\d[0-35-9]-\\d\\d-\\d\\d");
 
+		String string1 = "Jane's Birthday is 05-12-75\n" 
+						+ "Dave's Birthday is 11-04-68\n"
+						+ "John's Birthday is 04-28-73\n" 
+						+ "Joe's Birthday is 12-17-77";
+
+		// corresponde expressão regular à string e imprime as correspondências
+		Matcher matcher = expression.matcher(string1);
+
+		while (matcher.find())
+			System.out.println(matcher.group());
+		
 	}
 
-	public static boolean validateFirstName(String firstName)
-	{
-	return firstName.matches("[A-Z][a-zA-Z]*");
+	public static boolean validateFirstName(String firstName){
+		return firstName.matches("[A-Z][a-zA-Z]*");
 	}
 
 	// valida sobrenome
