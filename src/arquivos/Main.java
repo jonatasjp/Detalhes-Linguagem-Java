4114188a96ca6d42e6811d8d7f94e433b853a7b8
Jonatas Luis <jonatasluis.unipe@gmail.com>
2017-12-21 21:48:11 -0200

    trabalhando com arquivos

--- src/arquivos/Main.java
+++ src/arquivos/Main.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class Maain {
+public class Main {
 
 	public static void main(String[] args) throws IOException {
 		
