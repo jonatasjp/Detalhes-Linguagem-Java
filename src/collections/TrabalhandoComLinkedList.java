35e22f0257c77aa991f5b8929dba050690590234
Jonatas Luis <jonatasluis.unipe@gmail.com>
2017-12-28 21:28:37 -0200

    trabalhando com linkedlist

--- src/collections/TrabalhandoComLinkedList.java
+++ src/collections/TrabalhandoComLinkedList.java
@@ -1,5 +1,6 @@
 package collections;
 
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
@@ -27,6 +28,18 @@ public class TrabalhandoComLinkedList {
 		
 		for (String cor : listaCores) {
 			System.out.println(cor);
+		}
+		
+		String[] colors = {"preto", "azul", "amarelo"};
+		LinkedList<String> links = new LinkedList<>(Arrays.asList(colors));
+		links.addLast("vermelho");
+		links.add(3, "verde");
+		links.addFirst("ciano");
+		
+		colors = links.toArray(new String[links.size()]);
+		System.out.println("--------------------------");
+		for (String string : colors) {
+			System.out.println(string);
 		}
 	}
 
