c68b4f5a31dba3a1c84ec8da7df18c98a2c175d6
Jonatas Luis <jonatasluis.unipe@gmail.com>
2018-01-03 21:35:55 -0200

    trabalhando com stack

--- src/collections/TrabalhandoComStack.java
+++ src/collections/TrabalhandoComStack.java
@@ -8,6 +8,7 @@ public class TrabalhandoComStack {
 		
 		Stack<Integer> pilha = new Stack<>();
 		
+		//EMPILHANDO
 		pilha.push(1);
 		pilha.push(2);
 		pilha.push(3);
@@ -25,5 +26,22 @@ public class TrabalhandoComStack {
 		while(!pilha.isEmpty()) {
 			System.out.println(pilha.pop());
 		}
+
+		//EMPILHANDO NOVAMENTE
+		pilha.push(1);
+		pilha.push(2);
+		pilha.push(3);
+		pilha.push(4);
+		pilha.push(5);
+
+		System.out.println("---------------");
+		
+		//DESEMPILHANDO 2º forma
+		while(!pilha.isEmpty()) {
+			System.out.println(pilha.peek());
+			pilha.pop();
+		}
+		
+		
 	}
 }
