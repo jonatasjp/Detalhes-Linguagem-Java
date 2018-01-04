4eb95b2fe947befad09c0f4e5069f6e1bf38f4b7
Jonatas Luis <jonatasluis.unipe@gmail.com>
2018-01-03 22:04:24 -0200

    trabalhando com filas

--- src/collections/TrabalhandoComQueue.java
+++ src/collections/TrabalhandoComQueue.java
@@ -8,14 +8,28 @@ public class TrabalhandoComQueue {
 		
 		PriorityQueue<Double> fila = new PriorityQueue<>();
 		
+		//incluindo elementos na fila
 		fila.offer(1D);
 		fila.offer(2D);
 		fila.offer(3D);
 		fila.offer(4D);
 		fila.offer(5D);
 		
+		//imprimindo
+		for (Double double1 : fila) {
+			System.out.println(double1);
+		}
+		
+		System.out.println("------------");
+		
+		//retirando elementos da fila
 		while(!fila.isEmpty()) {
 			System.out.println(fila.poll());
 		}
+
+		System.out.println("------------");
+		
+		//deve ser 0
+		System.out.println(fila.size());
 	}
 }
