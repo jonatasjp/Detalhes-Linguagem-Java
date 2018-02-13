5dbe86762d58dadc8aab06c1b4360527c666ab59
Jonatas Luis <jonatasluis.unipe@gmail.com>
2018-02-13 14:23:56 -0200

    classe anonima

---
+++ src/inner_class/ClasseAnonimaTest.java
@@ -0,0 +1,43 @@
+package inner_class;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import generics.classes.Carro;
+
+class Animal {
+	
+	public void andar(){
+		System.out.println("andando");
+	}
+}
+public class ClasseAnonimaTest {
+
+	public static void main(String[] args) {
+		Animal a = new Animal() {
+			@Override
+			public void andar() {
+				System.out.println("andando anonimo");
+			}
+		};
+		a.andar();
+		
+		List<Carro> carros = new ArrayList<>();
+		carros.add(new Carro("bmw"));
+		carros.add(new Carro("audi"));
+		
+		Collections.sort(carros, new Comparator<Carro>() {
+			@Override
+			public int compare(Carro o1, Carro o2) {
+				return o1.getNome().compareTo(o2.getNome());
+			}
+		});
+		
+		carros.forEach(carro -> {
+			System.out.println(carro);
+		});
+	}
+	
+}
