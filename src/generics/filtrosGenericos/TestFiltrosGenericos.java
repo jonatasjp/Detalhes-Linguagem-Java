3e3fc23aba268860a4d59636a2fcdb853801b503
Jonatas Luis <jonatasluis.unipe@gmail.com>
2018-02-15 08:33:46 -0200

    filtro genérico

--- src/generics/filtrosGenericos/TestFiltrosGenericos.java
+++ src/generics/filtrosGenericos/TestFiltrosGenericos.java
@@ -14,18 +14,18 @@ public class TestFiltrosGenericos {
 		List<Carro> carros = Arrays.asList(new Carro("gol", "vermelho"), new Carro("bmw", "preto"),
 				new Carro("gol", "amarelo"), new Carro("jetta", "vermelho"));
 
-		System.out.println(filtrarCarros(carros, carro -> carro.getNome().equals("gol")));
-		System.out.println(filtrarCarros(carros, carro -> carro.getCor().equals("vermelho")));
+		System.out.println(filtrarObjetos(carros, carro -> carro.getNome().equals("gol")));
+		System.out.println(filtrarObjetos(carros, carro -> carro.getCor().equals("vermelho")));
 
 	}
 
-	private static List<Carro> filtrarCarros(List<Carro> carros, Predicate<Carro> predicate) {
-		List<Carro> carrosFiltrados = new ArrayList<>();
-		for (Carro carro : carros) {
-			if (predicate.test(carro))
-				carrosFiltrados.add(carro);
+	private static <T> List<T> filtrarObjetos(List<T> lista, Predicate<T> predicate) {
+		List<T> filtrados = new ArrayList<>();
+		for (T objeto: lista) {
+			if (predicate.test(objeto))
+				filtrados.add(objeto);
 		}
-		return carrosFiltrados;
+		return filtrados;
 	}
 	
 }
