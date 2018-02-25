7e7c68a124fdc8bbcc83e5873bf5c2ccae6c8a11
Jonatas Luis <jonatasluis.unipe@gmail.com>
2018-02-25 09:46:41 -0300

    localdatetime

---
+++ src/datas/dateTimeAPI/LocalTimeTeste.java
@@ -0,0 +1,14 @@
+package datas.dateTimeAPI;
+
+import java.time.LocalTime;
+
+public class LocalTimeTeste {
+	
+	public static void main(String[] args) {
+		
+		LocalTime localTime = LocalTime.of(23, 59, 59);
+		System.out.println(localTime);
+		
+	}
+	
+}
