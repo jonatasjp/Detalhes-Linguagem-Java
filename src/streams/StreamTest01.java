360c5385e1ae48e59f7643e7e93d6130e16976da
Jonatas Luis <jonatasluis.unipe@gmail.com>
2018-03-14 20:27:07 -0300

    map e flatmap

--- src/streams/StreamTest01.java
+++ src/streams/StreamTest01.java
@@ -22,13 +22,14 @@ public class StreamTest01 {
 		List<String> palavras = Arrays.asList("Jonatas", "André");
 		//Fazendo a mesma coisa de dois modos diferentes
 		//basicamente transforma a lista de palavras em uma lista de strings com os caracteres das palavras
-		palavras.stream().flatMap( palavra -> Arrays.stream(palavra.split(""))).collect(Collectors.toList());
-		palavras.stream().map( palavra -> palavra.split("")).flatMap(Arrays::stream);
+		System.out.println(
+				palavras.stream().flatMap( palavra -> Arrays.stream(palavra.split(""))).collect(Collectors.toList()));
+		System.out.println(
+				palavras.stream().map( palavra -> palavra.split("")).flatMap(Arrays::stream).collect(Collectors.toList()));
 		
-		System.out.println(palavras.stream().map( palavra -> palavra.split("")).flatMap(Arrays::stream).collect(Collectors.toList()));
 		
 		System.out.println(palavras.stream().map(palavra -> palavra.length()).flatMapToInt( quantidade -> IntStream.of(quantidade)).sum());
-	
+		
 	}
 	
 }
